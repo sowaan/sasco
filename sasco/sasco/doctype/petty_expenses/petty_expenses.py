fd18dcde4ab16ea1f0077ddcb96ee0dca12e5865
amirbalochsowaan <amir.baloch@ahwaal.com>
2025-12-12 12:33:11 +0500

    Remove Petty Cash DocTypes

--- sasco/sasco/doctype/petty_expenses/petty_expenses.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Sowaan and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class PettyExpenses(Document):
-	pass
